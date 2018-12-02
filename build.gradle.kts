e0a8a7488f8c5d39b44b0c02aa780a36d5f605b7
Nicholas Yip <nicholasyip@obceit.cc>
2018-12-02 16:50:43 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+  maven
+  scala
+  `java-library`
+}
+val scalaCompilerPlugin: Configuration = configurations.create("scalaCompilerPlugin")
+
+tasks.getByName<Wrapper>("wrapper") {
+  gradleVersion = "4.10.2"
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+  jcenter()
+}
+dependencies {
+  scalaCompilerPlugin("org.scalamacros:paradise_2.12.7:2.1.1")
+  api("org.scala-lang:scala-library:2.12.7")
+  api("co.fs2:fs2-io_2.12:1.0.0")
+}
+
+tasks.withType<ScalaCompile> {
+  scalaCompileOptions.additionalParameters = listOf(
+      "-Xplugin:" + scalaCompilerPlugin.asPath,
+      "-Ypartial-unification",
+      "-language:higherKinds")
+}
