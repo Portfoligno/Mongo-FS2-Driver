7f36b7bcf15e0180deebf1af9f06d5f914cc2890
Nicholas Yip <nicholasyip@obceit.cc>
2018-12-03 04:26:05 +0000

    Add Circe Core

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
   scalaCompilerPlugin("org.scalamacros:paradise_2.12.7:2.1.1")
   api("org.scala-lang:scala-library:2.12.7")
   api("co.fs2:fs2-io_2.12:1.0.0")
+  api("io.circe:circe-core_2.12:0.10.1")
 
   implementation("org.mongodb:mongodb-driver-reactivestreams:1.10.0")
 }
